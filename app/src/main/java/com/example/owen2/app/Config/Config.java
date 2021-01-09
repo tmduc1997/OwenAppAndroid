8a85ab0d4c0c8844e4161a30b4e006509702bd55
tmduc1997 <minhminhducit@gmail.com>
2021-01-09 16:53:33 +0700

    delete notification

--- app/src/main/java/com/example/owen2/app/Config/Config.java
+++ app/src/main/java/com/example/owen2/app/Config/Config.java
@@ -1,6 +1,5 @@
 package com.example.owen2.app.Config;
 
 public class Config {
-    public static final String PAYPAL_CLIENT_ID="";
-            //"AbGkQ2LeUA8AR07VALTQDZvrVl-Dfd3MXKufL2xpqzv3SIef0HOdhXKP8YHN9MtwqIZYe_4YaaKE2yvH";
+    public static final String PAYPAL_CLIENT_ID="AbGkQ2LeUA8AR07VALTQDZvrVl-Dfd3MXKufL2xpqzv3SIef0HOdhXKP8YHN9MtwqIZYe_4YaaKE2yvH";
 }
